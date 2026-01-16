ae8eca92a2dde98648f199265090542ecbbade20
neki-or <somaschini.marco2@gmail.com>
2026-01-16 23:00:23 +0100

    fix: correct word replacement keybing by removing useless chars

--- lua/neki/remap.lua
+++ lua/neki/remap.lua
@@ -2,7 +2,7 @@
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Replace current word
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>//gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/]])
 
 -- ALIAS
 -- Same key as _ which is the complementary motion and easier to press than '<S-4>'
