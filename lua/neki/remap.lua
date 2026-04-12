f21fdbd3e083e9677fdc105c9fa7cbce2d91f7d2
neki-or <somaschini.marco2@gmail.com>
2026-04-12 15:59:59 +0200

    feat: use Oil file manager instead of netrw

--- lua/neki/remap.lua
+++ lua/neki/remap.lua
@@ -20,7 +20,7 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 
-vim.keymap.set('n', '<leader>e', '<cmd>:Ex<CR>' ,      { desc = 'Open file manager' })
+vim.keymap.set('n', '<leader>e', '<cmd>:Oil<CR>' ,     { desc = 'Open file manager' })
 vim.keymap.set('n', '<leader>t', '<cmd>:ter<CR>',      { desc = 'Open terminal mode' })
 vim.keymap.set('n', '<leader>f', ':find ',             { desc = 'Find file shortcut' })
 vim.keymap.set('n', '<leader>w', '<cmd>:w<CR>',        { desc = 'Save buffer' })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,2 +1,3 @@
 require 'plugins.theme'
 require 'plugins.lualine'
+require 'plugins.oil'
